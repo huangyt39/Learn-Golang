2e515dbeb14e806107e422a6a11de1236093d768
huangyt39 <824551061@qq.com>
2019-01-18 21:37:48 +0800

    try login

--- login.go
+++ login.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"encoding/json"
 	"fmt"
@@ -40,19 +39,26 @@ type Param struct {
 
 func main() {
 
-	getCaptchaPic("https://cas.sysu.edu.cn/cas/captcha.jsp")
+	cookie := getCaptchaPicAndCookie("https://cas.sysu.edu.cn/cas/captcha.jsp")
+	// fmt.Println(cookie)
 	token := getLoginHTML("https://cas.sysu.edu.cn/cas/login?service=https%3A%2F%2Fuems.sysu.edu.cn%2Fjwxt%2Fapi%2Fsso%2Fcas%2Flogin%3Fpattern%3Dstudent-login")
-	login("https://cas.sysu.edu.cn/cas/login?service=https%3A%2F%2Fuems.sysu.edu.cn%2Fjwxt%2Fapi%2Fsso%2Fcas%2Flogin%3Fpattern%3Dstudent-login", token)
+	login("https://cas.sysu.edu.cn/cas/login?service=https%3A%2F%2Fuems.sysu.edu.cn%2Fjwxt%2Fapi%2Fsso%2Fcas%2Flogin%3Fpattern%3Dstudent-login", token, cookie)
 	// getCourseList("https://uems.sysu.edu.cn/jwxt/choose-course-front-server/classCourseInfo/selectCourseInfo?_t=1547712166")
 
 }
 
-func getCaptchaPic(url string) {
+func getCaptchaPicAndCookie(url string) string {
 	captchaPic, err := http.Get(url)
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer captchaPic.Body.Close()
+
+	//get cookie
+	cookie := captchaPic.Header.Get("Set-Cookie")
+	endIndex := strings.Index(cookie, ";")
+
+	//save captcha picture
 	pix, err := ioutil.ReadAll(captchaPic.Body)
 	if err != nil {
 		fmt.Println(err)
@@ -68,13 +74,15 @@ func getCaptchaPic(url string) {
 	} else {
 		fmt.Println("Captcha has been saved")
 	}
+
+	return cookie[:endIndex]
 }
 
-func login(loginURL string, token string) {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter captcha: ")
-	captchaCode, _ := reader.ReadString('\n')
-	// captchaCode := "test"
+func login(loginURL string, token string, cookie string) {
+	// reader := bufio.NewReader(os.Stdin)
+	// fmt.Print("Enter captcha: ")
+	// captchaCode, _ := reader.ReadString('\n')
+	captchaCode := "test"
 
 	client := &http.Client{}
 
@@ -86,16 +94,18 @@ func login(loginURL string, token string) {
 
 	req.ParseForm()
 	req.Form.Add("username", "huangyt39")
-	req.Form.Add("password", "ChAnrd7ler")
+	req.Form.Add("password", "chAnrd7ler")
 	req.Form.Add("captcha", captchaCode)
 	req.Form.Add("execution", token)
+	req.Form.Add("_eventId", "submit")
 
 	req.Header.Set("Accept", "*/*")
 	req.Header.Set("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36(KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36")
-	req.Header.Set("origin", "https://uems.sysu.edu.cn")
-	req.Header.Set("host", "uems.sysu.edu.cn")
+	req.Header.Set("origin", "https://cas.sysu.edu.cn")
+	req.Header.Set("host", "cas.sysu.edu.cn")
 	req.Header.Set("connection", "keep-alive")
 	req.Header.Set("accept-language", "zh-CN,zh;q=0.9")
+	req.Header.Set("cookie", cookie)
 
 	resp, err := client.Do(req)
 	if err != nil {
@@ -104,6 +114,7 @@ func login(loginURL string, token string) {
 	defer resp.Body.Close()
 	b, err := ioutil.ReadAll(resp.Body)
 	fmt.Println(string(b))
+	fmt.Println(resp.StatusCode)
 }
 
 func getCourseList(courseListURL string) {
